632af7d74624246a6699756c8c6ce93782c5a1c0
Andrey Bakuta <anb@anahoret.com>
2017-07-07 09:23:21 +0300

    Add more tests

--- chapter-1/ex-1-8.rkt
+++ chapter-1/ex-1-8.rkt
@@ -25,5 +25,8 @@
 (module+ test
   (require rackunit)
 
+  (check-= 300.0 (cubert 27000000) 0.0001)
+  (check-= 0.02 (cubert 0.000008) 0.000000001)
+  (check-= 15 (cubert 3375) 0.0001)
   (check-= 2 (cubert 8) 0.001)
   (check-= 3 (cubert 27) 0.001))
